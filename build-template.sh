390424241f21f8c04b26125d4f626fe835c03401
Zheng Can <zc3593835@gmail.com>
2019-05-23 13:28:53 +0800

    update Dockerfile

--- build-template.sh
+++ build-template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Base Images
-sed -e 's/FROM .*/FROM openjdk:8-stretch/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' Dockerfile > 8/Dockerfile
-sed -e 's/FROM .*/FROM openjdk:11-stretch/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' Dockerfile > 11/Dockerfile
+sed -e 's/FROM .*/FROM openjdk:8-stretch/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' Dockerfile > 8/Dockerfile_base
+sed -e 's/FROM .*/FROM openjdk:11-stretch/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' Dockerfile > 11/Dockerfile_base
 
 # Warm Up
-sed -e 's/FROM .*/FROM zhengcan\/scala-sbt-node:8/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' warmup/Dockerfile > 8/Dockerfile_warmup
-sed -e 's/FROM .*/FROM zhengcan\/scala-sbt-node:11/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' warmup/Dockerfile > 11/Dockerfile_warmup
+sed -e 's/FROM .*/FROM zhengcan\/scala-sbt-node:8_base/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' warmup/Dockerfile > 8/Dockerfile
+sed -e 's/FROM .*/FROM zhengcan\/scala-sbt-node:11_base/g' -e 's/# Dockerfile Template #/# Auto Generated File#/g' warmup/Dockerfile > 11/Dockerfile
 
 
 
