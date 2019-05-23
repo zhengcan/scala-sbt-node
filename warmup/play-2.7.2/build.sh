74d91a0683ae11c65e91b23c6740076250d873e6
Zheng Can <zc3593835@gmail.com>
2019-05-23 18:34:15 +0800

    update Dockerfile

--- warmup/play-2.7.2/build.sh
+++ warmup/play-2.7.2/build.sh
@@ -2,5 +2,6 @@
 
 echo "Build Play 2.7.2"
 
+sed -e "s/sbt\.version=.*/sbt.version=${SBT_VERSION}/g" project/build.properties > project/build.properties
 sed -e "s/\${SCALA_VERSION}/${SCALA_VERSION}/g" -e "s/\${SCALA_VERSION_CROSS}/${SCALA_VERSION_CROSS}/g" build.tmpl > build.sbt
 sbt "+ dist"
