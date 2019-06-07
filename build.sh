a998e00703d1e32282f341762014e31b59dcdb9b
Zheng Can <zc3593835@gmail.com>
2019-06-07 20:42:19 +0800

    add push command

--- build.sh
+++ build.sh
@@ -9,6 +9,12 @@ docker build -f base/Dockerfile.8  --build-arg NODE_VERSION="12.4.0"  -t zhengca
 docker build -f base/Dockerfile.8  --build-arg NODE_VERSION="10.16.0" -t zhengcan/scala-sbt-node:8-base-node10  base
 docker tag      zhengcan/scala-sbt-node:11-base-node12                   zhengcan/scala-sbt-node:11-base
 docker tag      zhengcan/scala-sbt-node:8-base-node12                    zhengcan/scala-sbt-node:8-base
+docker push     zhengcan/scala-sbt-node:11-base-node12
+docker push     zhengcan/scala-sbt-node:11-base-node10
+docker push     zhengcan/scala-sbt-node:11-base
+docker push     zhengcan/scala-sbt-node:8-base-node12
+docker push     zhengcan/scala-sbt-node:8-base-node10
+docker push     zhengcan/scala-sbt-node:8-base
 
 # Main Images
 docker build -f main/Dockerfile.11                -t zhengcan/scala-sbt-node:11-node12 main
@@ -18,10 +24,19 @@ docker build -f main/Dockerfile.8-node10          -t zhengcan/scala-sbt-node:8-n
 docker tag      zhengcan/scala-sbt-node:11-node12    zhengcan/scala-sbt-node:11
 docker tag      zhengcan/scala-sbt-node:8-node12     zhengcan/scala-sbt-node:8
 docker tag      zhengcan/scala-sbt-node:11           zhengcan/scala-sbt-node:latest
+docker push     zhengcan/scala-sbt-node:11-node12
+docker push     zhengcan/scala-sbt-node:11-node10
+docker push     zhengcan/scala-sbt-node:11
+docker push     zhengcan/scala-sbt-node:8-node12
+docker push     zhengcan/scala-sbt-node:8-node10
+docker push     zhengcan/scala-sbt-node:8
+docker push     zhengcan/scala-sbt-node:latest
 
 # Cache Expo
-docker build -f cache-expo/Dockerfile.11 -t zhengcan/scala-sbt-node:11-expo cache-expo
-docker build -f cache-expo/Dockerfile.8  -t zhengcan/scala-sbt-node:8-expo cache-expo
+docker build -f cache-expo/Dockerfile.11        -t zhengcan/scala-sbt-node:11-expo cache-expo
+docker build -f cache-expo/Dockerfile.8         -t zhengcan/scala-sbt-node:8-expo cache-expo
+docker push     zhengcan/scala-sbt-node:11-expo
+docker push     zhengcan/scala-sbt-node:8-expo
 
 
 
